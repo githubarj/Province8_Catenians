5adb8075a32f51746d16f7fe43062916fa648b18
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-02-23 18:47:01 +0300

    server side scaffolding started

--- client/src/Components/Pages/Register.jsx
+++ client/src/Components/Pages/Register.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Register() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   return (
-    <form className='register'>
-        <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="text" placeholder="password" />
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUserName(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
       <button>Register</button>
     </form>
   );
 }
 
-export default Register
+export default Register;
